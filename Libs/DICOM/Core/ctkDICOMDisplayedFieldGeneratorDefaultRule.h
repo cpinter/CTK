e10852a8bd10c48f474bd73382edcfba1a2db938
Andras Lasso <lasso@cs.queensu.ca>
2013-05-22 15:05:52 -0400

    Added initial implemntation for default displayed field generator rule
    # Conflicts:
    #	Libs/DICOM/Core/ctkDICOMDisplayedFieldGeneratorAbstractRule.h

--- Libs/DICOM/Core/ctkDICOMDisplayedFieldGeneratorDefaultRule.h
+++ Libs/DICOM/Core/ctkDICOMDisplayedFieldGeneratorDefaultRule.h
@@ -90,7 +90,18 @@ public:
 
   virtual void getDisplayFieldsForInstance(QMap<QString, QString> cachedTags, QMap<QString, QString> &displayFieldsForCurrentSeries, QMap<QString, QString> &displayFieldsForCurrentStudy, QMap<QString, QString> &displayFieldsForCurrentPatient)
   {
-    // TODO: implement this
+    displayFieldsForCurrentPatient["PatientName"]=cachedTags[dicomTagToString(DCM_PatientName)];    
+    displayFieldsForCurrentPatient.insert("PatientID",cachedTags[dicomTagToString(DCM_PatientID)]);
+
+    displayFieldsForCurrentStudy.insert("StudyDescription",cachedTags[dicomTagToString(DCM_StudyDescription)]);    
+    displayFieldsForCurrentStudy.insert("StudyDate",cachedTags[dicomTagToString(DCM_StudyDate)]);    
+    displayFieldsForCurrentStudy.insert("ModalitiesInStudy",cachedTags[dicomTagToString(DCM_ModalitiesInStudy)]);    
+    displayFieldsForCurrentStudy.insert("InstitutionName",cachedTags[dicomTagToString(DCM_InstitutionName)]);    
+    displayFieldsForCurrentStudy.insert("ReferringPhysician",cachedTags[dicomTagToString(DCM_ReferringPhysicianName)]);    
+
+    displayFieldsForCurrentSeries.insert("SeriesNumber",cachedTags[dicomTagToString(DCM_SeriesNumber)]);    
+    displayFieldsForCurrentSeries.insert("SeriesDescription",cachedTags[dicomTagToString(DCM_SeriesDescription)]);    
+    displayFieldsForCurrentSeries.insert("Modality",cachedTags[dicomTagToString(DCM_Modality)]);    
   }
 
   virtual void mergeDisplayFieldsForInstance(
@@ -100,9 +111,19 @@ public:
     const QMap<QString, QString> &emptyFieldNamesSeries, const QMap<QString, QString> &emptyFieldNamesStudy, const QMap<QString, QString> &emptyFieldNamesPatient
     )
   {
-    // TODO: implement this
-  }
+    MergeExpectSameValue("PatientName", initialFieldsPatient, newFieldsPatient, mergedFieldsPatient);
+    MergeExpectSameValue("PatientID", initialFieldsPatient, newFieldsPatient, mergedFieldsPatient);
 
+    MergeConcatenate("StudyDescription", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+    MergeExpectSameValue("StudyDate", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+    MergeConcatenate("ModalitiesInStudy", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+    MergeExpectSameValue("InstitutionName", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+    MergeConcatenate("ReferringPhysician", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+
+    MergeExpectSameValue("SeriesNumber", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+    MergeConcatenate("SeriesDescription", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+    MergeExpectSameValue("Modality", initialFieldsStudy, newFieldsStudy, mergedFieldsStudy);
+  }
 
 };
 

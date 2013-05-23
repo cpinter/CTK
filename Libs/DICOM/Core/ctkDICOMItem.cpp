cbb6ad57014f6b8930cafb1fb9e2f750c052f46f
Csaba Pinter <pinter@cs.queensu.ca>
2013-05-23 13:42:06 -0400

    Inserting one patient/study/series has the expected result in the Display* tables
    # Conflicts:
    #	Libs/DICOM/Core/ctkDICOMDatabase.cpp
    #	Libs/DICOM/Core/ctkDICOMDataset.h
    #	Libs/DICOM/Core/ctkDICOMItem.cpp

--- Libs/DICOM/Core/ctkDICOMItem.cpp
+++ Libs/DICOM/Core/ctkDICOMItem.cpp
@@ -939,7 +939,12 @@ QString ctkDICOMItem::TagKey( const DcmTag& tag )
   return QString("(%1,%2)").arg( tag.getGroup(), 4, 16, QLatin1Char('0')).arg( tag.getElement(), 4, 16, QLatin1Char('0') );
 }
 
-QString ctkDICOMItem::TagDescription( const DcmTag& tag )
+QString ctkDICOMDataset::TagKeyStripped( const DcmTag& tag )
+{
+  return QString("%1,%2").arg( tag.getGroup(), 4, 16, QLatin1Char('0')).arg( tag.getElement(), 4, 16, QLatin1Char('0') );
+}
+
+QString ctkDICOMDataset::TagDescription( const DcmTag& tag )
 {
   if (!dcmDataDict.isDictionaryLoaded())
     return QString("<no DICOM dictionary loaded. application broken>");
